5bc57895ddbe0cbb30c71217122acd5b3b17db20
IgorAgf <igor.agf@mail.ru>
2022-01-12 16:29:18 +0400

    pervoe zadanie

--- app.js
+++ app.js
@@ -2,10 +2,16 @@
 let numberofFilms = prompt('Сколько фильмов Вы уже посмотрели?', 0);
 let personalMovieDB = {
     count: numberofFilms,
-    movies: '',
-    actors: '',
-    genres: new Array(),
+    movies: {},
+    actors: {},
+    genres: [],
     privat: false
 };
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
