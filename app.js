bdbd4f6b5247b0f6084779b3f50b15c7b369579b
IgorAgf <igor.agf@mail.ru>
2022-01-13 16:00:31 +0400

    2 DP zadanie

--- app.js
+++ app.js
@@ -7,20 +7,40 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-let a = '';
-let b = '';
-for (let i = 0; i < 2; i++) {
-    do {
-        a = prompt((i + 1) + ' Один из последних просмотренных фильмов?', '');
-        if (a == null) { a = ''; }
-        if (a.length >= 50) { a = '' }
-    } while (a == '');
-    do {
+// let a = '';
+// let b = '';
+// for (let i = 0; i < 2; i++) {
+//     do {
+//         a = prompt((i + 1) + ' Один из последних просмотренных фильмов?', '');
+//         if (a == null) { a = ''; }
+//         if (a.length >= 50) { a = '' }
+//     } while (a == '');
+//     do {
+//         b = prompt('На сколько оцените его?', '');
+//         if (b == null) { b = ''; }
+//     } while (b == '');
+//     personalMovieDB.movies[a] = b;
+// }
+
+// 2 способ
+// let i = 0;
+// while (i < 2) {
+//     const a = prompt((i + 1) + ' Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+//     if (a != '' && a != null && b != '' && b != null && a.length < 50) {
+//         i++;
+//     }
+
+// 3 Способ
+let i = 0;
+do {
+    const a = prompt((i + 1) + ' Один из последних просмотренных фильмов?', ''),
         b = prompt('На сколько оцените его?', '');
-        if (b == null) { b = ''; }
-    } while (b == '');
-    personalMovieDB.movies[a] = b;
-}
+    if (a != '' && a != null && b != '' && b != null && a.length < 50) {
+        i++;
+    }
+} while (i < 2);
+
 if (personalMovieDB.count < 10) {
     alert('Просмотренно довольно мало фильмов')
 } else {
