51460599a479c942bca19b7409f2335dfed59ede
IgorAgf <igor.agf@mail.ru>
2022-02-02 14:18:40 +0400

    konec 3 practiki

--- script3.js
+++ script3.js
@@ -12,23 +12,65 @@ genres
 P.S. Функции вызывать не обязательно*/
 
 'use strict';
+
 let numberofFilms;
 
 function start() {
     numberofFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
-    // while (numberofFilms == '' || numberofFilms == null || isNaN(numberofFilms)) {
-    //     numberofFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
-    // }
-    start();
+    while (numberofFilms == '' || numberofFilms == null || isNaN(numberofFilms)) {
+        numberofFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+    }
+}
+start();
+const personalMovieDB = {
+    count: numberofFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
 
-    const personalMovieDB = {
-        count: numberofFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
+function rememberMyFilms() {
+    let i = 0;
+    do {
+        const a = prompt((i + 1) + ' Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
+        if (a != '' && a != null && b != '' && b != null && a.length < 50) {
+            i++;
+            personalMovieDB.movies[a] = b;
+        }
+    } while (i < 2);
+
+}
+// rememberMyFilms();
+
+function detectPersonLevel() {
+    if (personalMovieDB.count < 10) {
+        alert('Просмотренно довольно мало фильмов')
+    } else {
+        if (personalMovieDB.count < 30) {
+            alert('Вы класический зритель')
+        } else { alert('Вы киноман!') }
     }
 
 }
+// detectPersonLevel();
 
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    }
+}
+
+function writeYourGenres() {
+    let i = 0;
+    do {
+        personalMovieDB.genres[i] = prompt('Ваш любимый жанр под номером' + (i + 1) + '?', 'Комедия');
+        i++;
+    } while (i < 3);
+}
+
+writeYourGenres();
+
+showMyDB();
 // Код возьмите из предыдущего домашнего задания
