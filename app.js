4849d23aee7cfc981cfafb0707b86f77e290f46f
IgorAgf <igor.agf@mail.ru>
2022-02-08 16:22:19 +0400

    DZ 4

--- app.js
+++ app.js
@@ -1,52 +1,55 @@
 'use strict'
-const numberofFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
 const personalMovieDB = {
     count: numberofFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
-// let a = '';
-// let b = '';
-// for (let i = 0; i < 2; i++) {
-//     do {
-//         a = prompt((i + 1) + ' Один из последних просмотренных фильмов?', '');
-//         if (a == null) { a = ''; }
-//         if (a.length >= 50) { a = '' }
-//     } while (a == '');
-//     do {
-//         b = prompt('На сколько оцените его?', '');
-//         if (b == null) { b = ''; }
-//     } while (b == '');
-//     personalMovieDB.movies[a] = b;
-// }
+    privat: false,
 
-// 2 способ
-// let i = 0;
-// while (i < 2) {
-//     const a = prompt((i + 1) + ' Один из последних просмотренных фильмов?', ''),
-//         b = prompt('На сколько оцените его?', '');
-//     if (a != '' && a != null && b != '' && b != null && a.length < 50) {
-//         i++;
-//     }
-
-// 3 Способ
-let i = 0;
-do {
-    const a = prompt((i + 1) + ' Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
-    if (a != '' && a != null && b != '' && b != null && a.length < 50) {
-        i++;
+    start() {
+        numberofFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+        while (numberofFilms == '' || numberofFilms == null || isNaN(numberofFilms)) {
+            numberofFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms() {
+        let i = 0;
+        do {
+            const a = prompt((i + 1) + ' Один из последних просмотренных фильмов?', ''),
+                b = prompt('На сколько оцените его?', '');
+            if (a != '' && a != null && b != '' && b != null && a.length < 50) {
+                i++;
+                personalMovieDB.movies[a] = b;
+            }
+        } while (i < 2);
+    },
+    detectPersonLevel() {
+        if (personalMovieDB.count < 10) {
+            alert('Просмотренно довольно мало фильмов')
+        } else {
+            if (personalMovieDB.count < 30) {
+                alert('Вы класический зритель')
+            } else { alert('Вы киноман!') }
+        }
+    },
+    showMyDB() {
+        if (personalMovieDB.privat == false) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres() {
+        let i = 0;
+        do {
+            personalMovieDB.genres[i] = prompt('Ваш любимый жанр под номером' + (i + 1) + '?', 'Комедия');
+            i++;
+        } while (i < 3);
+    },
+    toggleVisibleMyDB(logic) {
+        if (personalMovieDB.privat == true) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
     }
-} while (i < 2);
-
-if (personalMovieDB.count < 10) {
-    alert('Просмотренно довольно мало фильмов')
-} else {
-    if (personalMovieDB.count < 30) {
-        alert('Вы класический зритель')
-    } else { alert('Вы киноман!') }
 }
-
-console.log(personalMovieDB);
